b6abd8c28d101033e5ff48ddfe11911cfa80ff1a
Ganesh Mohane <ganeshmohane5@gmail.com>
2023-10-16 23:14:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-
-import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 from io import BytesIO
 import base64 
